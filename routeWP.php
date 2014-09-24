74043608149806293b8086b173886cd1e1f22390
Troels Abrahamsen <troels@blck.dk>
2014-09-24 16:06:29 +0200

    routeWP via submodule

--- routeWP.php
+++ routeWP.php
@@ -9,38 +9,75 @@ class routeWP {
 
 	function __construct(){
 		add_filter('request', array($this, 'filter_request'), 999, 1);
+		add_filter('query_vars', array($this, 'setup_query_vars'), 999, 1);
+		add_filter('parse_query', array($this, 'parse_query'), 999, 1);
+		add_filter('template_include', array($this, 'handle_request_template'), 999, 1);
 	}
 
-	function add_route($pattern, $args = array(), $template){
-		$this->routes[$pattern] = array('args' => $args, 'template' => $template);
+	function add_route($args){
+
+		$defaults = array(
+			'template'	 	=> false,
+			'query_vars' 	=> array(),
+			'support' 		=> array()
+			);
+
+		$args = array_merge($defaults, $args);
+
+		if(!$args['pattern'])
+			return false;
+
+		$this->routes[$args['pattern']] = array(
+			'query_vars' => $args['query_vars'], 
+			'template' => $args['template'],
+			'support' => $args['supports']
+			);
+
+	}
+
+	function get_route(){
+
+		if($this->route)
+			return $this->route;
+
+		$req = $_SERVER['REDIRECT_URL']; // This should probably be something else
+		if(!$req)
+			$req = '/';
+
+		foreach($this->routes as $pattern => $route){
+
+			if(preg_match($pattern, $req, $matches)){
+
+				if(is_array($route['query_vars'])){
+
+					foreach($route['query_vars'] as $key => $value){
+
+						if(preg_match('~^\$([\d]{1,3})$~', $value, $num)){
+							if($matches[$num[1]])
+								$route['query_vars'][$key] = $matches[$num[1]];
+							else
+								unset($route['query_vars'][$key]);
+						}
+
+					}
+
+				}
+
+				if(is_array($route['supports'])){
+
+				}
+
+				$this->route = $route;
+				return $this->route;
+			}
+		}
+
 	}
 
 	function filter_request($request){
 		
-		$req = $_SERVER['REDIRECT_URL'];
-
-		foreach($this->routes as $pattern => $route){
-			if(preg_match($pattern, $req, $matches)){
-				$request = array();
-				foreach($matches as $k => $match){
-					
-					if($k === 0) // Skip the first match as it contains the whole string
-						continue;
-
-					$index = $k -1;
-					/*
-					Now we'll save the regex matches in the request vars
-					named via the $route['args'] array;
-					*/
-					if($route['args'][$index])
-						$request[$route['args'][$index]] = $match;
-				}
-
-				$this->template = $route['template'];
-				add_action('template_include', array($this, 'handle_request_template'));
-
-				break;
-			}
+		if($route = $this->get_route()){
+			$request = array();
 		}
 
 		return $request;
@@ -49,7 +86,32 @@ class routeWP {
 
 
 	function handle_request_template($tmpl){
-		return $this->template;
+		if($route = $this->get_route())
+			if($route['template'])
+				$tmpl = $route['template'];
+
+		return $tmpl;
+	}
+
+	function setup_query_vars($query_vars){
+		if($route = $this->get_route()){
+			foreach($route['query_vars'] as $key => $value){
+				$query_vars[] = $key;
+			}
+		}
+		return $query_vars;
+	}
+
+	function parse_query($query){
+		
+		if($route = $this->get_route()){
+			foreach($route['query_vars'] as $key => $value){
+				$query->set($key, $value);
+				$query->query[$key] = $value;
+			}
+		}
+
+		return $query;
 	}
 
 }
